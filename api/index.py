759ffdc98570c4d05c4aa729fb5c5d6d3d201551
Youssef Dagher <youssef@local.dev>
2026-06-07 22:05:40 +0300

    Rename public/ to static/ so Vercel bundles it with the function

--- api/index.py
+++ api/index.py
@@ -19,7 +19,7 @@ from flask import Flask, Response, jsonify, make_response, request
 
 from bot.supabase_db import SupabaseDB, SupabaseError
 
-_public = os.path.join(_root, "public")
+_public = os.path.join(_root, "static")
 app = Flask(__name__)
 
 
